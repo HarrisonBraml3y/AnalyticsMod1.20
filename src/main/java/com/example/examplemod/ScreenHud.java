ac8cb0f4976f76829aced82a7d536dfb56fe5a3c
HarrisonBraml3y <harrison-bramley@hotmail.com>
2023-08-14 23:09:18 +0100

    14/08/2023

--- src/main/java/com/example/examplemod/ScreenHud.java
+++ src/main/java/com/example/examplemod/ScreenHud.java
@@ -9,6 +9,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import org.apache.logging.log4j.core.pattern.TextRenderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
+
 public class ScreenHud extends Screen {
 
     protected ScreenHud(Component Hud){
@@ -17,15 +20,18 @@ public class ScreenHud extends Screen {
     }
 
     @Override
-    public void render(int mouseX, int mouseY, float partialTicks){
-        super.render(mouseX, mouseY, partialTicks);
+    public void render(GuiGraphics Graphics, int mouseX, int mouseY, float partialTicks){
+        super.render(Graphics, mouseX, mouseY, partialTicks);
+
+        //GuiGraphics GuiGraphics = new GuiGraphics(Stack);
+
     }
 
 
 
     protected void Init(){
         super.init();
-        this.addRenderableOnly(new EditBox(font, 10, 10, 20, 20, Component.literal"Test"));
+        this.addRenderableOnly(new EditBox(font, 10, 10, 20, 20, ));
 
     }
 
