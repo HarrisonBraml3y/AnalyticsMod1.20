bba7f6d2fd2d8b390e2b6c3b5bc1d58fba714f59
HarrisonBraml3y <harrison-bramley@hotmail.com>
2023-08-23 21:23:57 +0100

    23/08/2023

--- src/main/java/com/example/examplemod/RenderGui.java
+++ src/main/java/com/example/examplemod/RenderGui.java
@@ -1,9 +1,10 @@
-/*package com.example.examplemod;
+package com.example.examplemod;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -17,6 +18,26 @@ import java.awt.*;
 
 public class RenderGui() {
 
+
+    @SubscribeEvent
+    void onRender(RenderGuiOverlayEvent RenderEvent){
+        if(RenderEvent.getOverlay() != VanillaGuiOverlay.DEBUG_TEXT.type()){
+            Render(RenderEvent.getGuiGraphics());
+        }
+
+
+
+    }
+
+    void Render(GuiGraphics Stack){
+        Minecraft Mc = Minecraft.getInstance();
+        if(Mc.options.renderDebug){
+            return;
+        }
+        Settings.ClientSettings
+
+
+    }
 
     RenderGuiOverlayEvent RenderEvent;
 
@@ -33,4 +54,3 @@ public class RenderGui() {
 
 }
 
-*/
